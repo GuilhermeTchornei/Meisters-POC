79235937985e50e2ef72690f1044327ee5f477f2
Guilherme Tchornei <guilhermetchornei@gmail.com>
2023-11-13 18:19:57 -0300

    refact: made service asynchronous

--- front-end/Javascript/components/index.js
+++ front-end/Javascript/components/index.js
@@ -4,7 +4,7 @@ function taskCard(task){
         <div id=${task.id}  class="task-card">
             <div>
                 <h1 class="${!status && "line-through"}">${task.title}</h1>
-                <button onclick="patchStatus(${task.id})">
+                <button type="button" onclick="patchStatus(${task.id})">
                     ${
                         status ? 
                         `<ion-icon name="radio-button-off-outline"></ion-icon>` : 
@@ -14,7 +14,7 @@ function taskCard(task){
             </div>
             <div>
                 <p class="${!status && "line-through"}">${task.description || "<i>No description</i>"}</p>
-                <button class="trash" onclick="deleteTask(${task.id})">
+                <button class="trash" type="button" onclick="deleteTask(${task.id})">
                     <ion-icon name="trash-outline"></ion-icon>
                 </button>
             </div>
@@ -28,7 +28,7 @@ function newTaskForm(){
             <input id="title" type="text" placeholder="Title..." maxlength="50" onchange="" required />
             <textarea id="description" placeholder="Description..." maxlength="255"></textarea>
             <button class="submit" type="submit">Submit</button>
-            <button class="cancel" onclick="closeForm()">Cancel</button>
+            <button class="cancel" type="button" onclick="closeForm()">Cancel</button>
         </form>
         `;
 }
